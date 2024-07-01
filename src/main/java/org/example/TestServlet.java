fb70345aaadd44efcdfeb52f351db52852bac3d6
Heybat <heybetmovlamov2005@gmail.com>
2024-07-01 17:23:01 +0100

    edited commit

--- src/main/java/org/example/TestServlet.java
+++ src/main/java/org/example/TestServlet.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 
@@ -17,11 +16,8 @@ public class TestServlet extends HttpServlet {
 
         try (PrintWriter pw = resp.getWriter()) {
             System.out.println("GET method called -> " + LocalDateTime.now());
-            if ("Hello".equalsIgnoreCase(name)) {
-                pw.write("Hello,"+ name +"!");
-            } else {
-                pw.write("Hello, World!");
-            }
+            String message = "Hello, " + ("Hello".equalsIgnoreCase(name) ? name : "World") + "!";
+            pw.write(message);
         } catch (IOException e) {
             e.printStackTrace();
         }
